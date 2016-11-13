1d70f117dc11bf5e47d8fd6e4b1a463540a6bb8a
arwinneil <arwinneil@gmail.com>
2016-11-13 18:27:52 +0400

    Faster Close Animation

--- SQL Generator/Animation.vb
+++ SQL Generator/Animation.vb
@@ -3,7 +3,7 @@
 
         Do
             Form.Opacity = Form.Opacity - 0.02
-            System.Threading.Thread.Sleep(4)
+            System.Threading.Thread.Sleep(3)
         Loop Until Form.Opacity = 0
 
     End Sub
